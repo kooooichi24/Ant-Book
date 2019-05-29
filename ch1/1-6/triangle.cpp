54132d06db3355456b42e17a34625564f833db19
古川広一 <ex.kouichi1224@gmail.com>
2019-05-29 11:02:24 +0900

    1-6 triangle

---
+++ ch1/1-6/triangle.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 100;
+int n, a[MAX_N];
+
+void solve() {
+  int ans = 0;
+
+  // 棒を重複して選ばないようi<j<kとなるように
+  for (int i=0; i<n; i++) {
+    for (int j=i+1; j<n; j++) {
+      for (int k=j+1; k<n; k++) {
+        int len = a[i] + a[j] + a[k];
+        int ma = max(a[i], max(a[j], a[k]));
+        int rest = len - ma;
+
+        if (ma < rest) {
+          ans = max(ans, len);
+        }
+      }
+    }
+  }
+    printf("%d\n", ans);
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i=0; i<n; i++) {
+    scanf("%d", &a[i]);
+  }
+  solve();
+}
