d93621aa0ec45bd3e8c2b0fa53b5eec4b544a24a
古川広一 <ex.kouichi1224@gmail.com>
2019-06-07 15:50:47 +0900

    dfs変更

--- ch2/2-1/dfs.cpp
+++ ch2/2-1/dfs.cpp
@@ -6,9 +6,15 @@ using namespace std;
 const int MAX_N = 20;
 int a[MAX_N], n, k;
 
+bool dfs(int i, int sum) {
+  if (i == n) return sum == k;
+  return (dfs(i + 1, sum) || dfs(i + 1, sum + a[i]));
+}
+
 
 void solve() {
-  
+  if (dfs(0, 0)) printf("Yes\n");
+  else printf("No\n");
 }
 
 int main() {
