24c18fc111b78eae511a7d4efa2a6cc2daa310e0
古川広一 <ex.kouichi1224@gmail.com>
2019-09-05 19:46:29 +0900

    学びが多いE問でした。

--- ch2/practice/ABC_139_E.cpp
+++ ch2/practice/ABC_139_E.cpp
@@ -1,32 +1,66 @@
-#include <iostream>
-#include <algorithm>
 #include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
 
 int main() {
-  int N; cin >> N;
-  vector<vector<int>> A(N, vector<int>(N, -1));
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      if (i == j) continue;
-      cin >> A[i][j];
+  // input
+  int n;
+  cin >> n;
+  vector<vector<int>> a(n, vector<int>(n-1));
+  rep(i,n) {
+    rep(j,n-1) {
+      cin >> a[i][j];
+      a[i][j]--;
+    }
+    reverse(a[i].begin(), a[i].end());
+  }
+
+  // Add to queue if matches are the same
+  vector<P> q;
+  auto check = [&](int i){
+    if (a[i].size() == 0) return;
+    int j = a[i].back();
+    if (a[j].back() == i) {
+      P p(i,j);
+      if (p.first > p.second) swap(p.first, p.second);
+      q.push_back(p);
+    }
+  };
+  
+  // initialization
+  rep(i,n) {
+    check(i);
+  }
+
+  // The process is repeated untill the queue for the next match is empty.
+  int day = 0;
+  while (q.size() > 0) {
+    day++;
+    sort(q.begin(), q.end());
+    q.erase(unique(q.begin(), q.end()), q.end());
+    vector<P> prevQ;
+    swap(prevQ, q);
+    for (P p : prevQ) {
+      int i = p.first, j = p.second;
+      a[i].pop_back();
+      a[j].pop_back();
+    }
+    for (P p : prevQ) {
+      int i = p.first, j = p.second;
+      check(i);
+      check(j);
     }
   }
 
-  int cnt = 1;
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      if (i == j) continue;
-
-      if (A[i][j] == cnt) {
-        if (A[i][j] == A[j][i]) {
-          A[i][j] = -1;
-          A[j][i] = -1;
-        } else {
-          
-        }
-      }
+  // output
+  rep(i,n) {
+    if (a[i].size() != 0) {
+      puts("-1");
+      return 0;
     }
   }
+  cout << day << endl;
   return 0;
 }
